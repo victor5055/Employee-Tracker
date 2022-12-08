3541b595501878e0d81557ebf3f744bceb1d68e0
Victor <victor5055@outlook.com>
2022-12-08 17:25:01 -0500

    Ading table employee.

--- db/schema.sql
+++ db/schema.sql
@@ -16,5 +16,17 @@ salary DECIMAL NOT NULL,
 department_id INT,
 FOREIGN KEY (department_id) 
 REFERENCES department(id) 
-ON DELETE CASCADE
+ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+
+role_id INT,
+FOREIGN KEY (role_id) 
+REFERENCES roles(id) ON DELETE SET NULL,
+
+manager_id INT
 );
