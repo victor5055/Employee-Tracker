2dd58173f2a9654ac8193f9527e88394a0a0ee5f
Victor <victor5055@outlook.com>
2022-12-08 17:33:02 -0500

    Adding differnet roles,salary and dpartment id.

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,15 @@ VALUES ("Sales"),
 ("Marketing and Advertising"),
 ("Human Resouces"),
 ("Information Technology");
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Associate", 5, 2),
+("Assistant Sales Manager", 6 2),
+("Store Manager", 6, 3),
+("General Marketing", 8, 5),
+("Business Partner", 5, 3),
+("Human Resource Director", 8, 5),
+("System Adminstrator", 9, 1),
+("IT Coordinator", 9, 1),
+("IT Manager", 6, 1);
